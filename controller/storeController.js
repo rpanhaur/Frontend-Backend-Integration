47a1ea92c51c2a4eebe34eeb20fd06c4009cfca3
rpanhaur <rpanhaur@gmail.com>
2025-05-22 08:48:16 +0545

    update in code

--- controller/storeController.js
+++ controller/storeController.js
@@ -50,3 +50,104 @@ exports.postStore = async function (req, res) {
     }
 
 }
+
+exports.deleteStore = async function (req, res) {
+
+    try {
+
+        const { id } = req.params
+
+        const result = await stores.destroy({
+            where: {
+                id: id
+            }
+        })
+
+        res.json({
+            message: 'Store data is Successfully deleted',
+            results: result
+        })
+
+
+
+    } catch (error) {
+        res.json({
+            message: 'Something is Wrong'
+        })
+
+
+    }
+
+
+}
+
+exports.updateStore = async function (req, res) {
+
+
+
+    try {
+
+        const id = req.params.id
+
+        const { storeName, storeOwner, storeAddress, storePhone } = req.body
+
+        const result = await stores.update({ storeName, storeOwner, storeAddress, storePhone }, {
+            where: {
+                id: id
+            }
+        })
+
+
+
+        res.json({
+            message: 'Store is updated Successfully'
+        })
+
+    } catch (error) {
+
+        res.json({
+            message: 'Somthing is wrong'
+        })
+
+    }
+
+
+
+
+
+}
+
+exports.singlePage = async function (req, res) {
+
+    try {
+
+        const id = req.params.id
+
+
+
+
+        const result = await stores.findByPk(id)
+
+        res.json({
+            message: 'Single page is fetched Successfully',
+            result
+        })
+
+    } catch (error) {
+
+        res.json({
+            message: 'Someting is wrong'
+        })
+
+    }
+
+
+
+
+
+}
+
+
+
+
+
--- routes/storeRouter.js
+++ routes/storeRouter.js
@@ -1,8 +1,8 @@
-const { fetchStore, postStore } = require('../controller/storeController')
+const { fetchStore, postStore, deleteStore, updateStore, singlePage } = require('../controller/storeController')
 
 const router = require('express').Router()
 
 router.route('/store').get(fetchStore).post(postStore)
-// router.route('/store/:id').delete().patch().get()
+router.route('/store/:id').delete(deleteStore).patch(updateStore).get(singlePage)
 
 module.exports = router
